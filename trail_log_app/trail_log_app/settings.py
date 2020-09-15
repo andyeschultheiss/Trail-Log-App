373303065176293912cf355f9cc3d5ab1be3301b
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-09-15 11:43:05 -0700

    correct path to trails app in settings.py

--- trail_log_app/trail_log_app/settings.py
+++ trail_log_app/trail_log_app/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'trails',
+    'trail_log_app.trails',
     'rest_framework',
     'frontend',
     'knox',
